350f0d5aeadaee828af26916bae3bb5e06b641ce
Aaron Ackerman <theron17@gmail.com>
2014-10-19 18:18:34 -0500

    Removed extra examples and create directory for talk about flux concepts

--- react-concepts/examples/file-stat-example.js
+++
@@ -1,9 +0,0 @@
-var Q    = require('q');
-var fs   = require('fs');
-var stat = Q.denodeify(fs.stat);
-
-Q.spawn(function *(){
-  var stat1 = yield stat('index.html');
-  var stat2 = yield stat('notes.md');
-  console.log(stat1.size, stat2.size);
-});
